0c4dc76fc57975f87115b6891b9e25522e4c9195
Zhamanta Chavez <zhamantach04@gmail.com>
2025-02-07 00:42:18 -0600

    Deleted unnecessary

--- Assets/OriginalSlot.cs
+++ Assets/OriginalSlot.cs
@@ -10,25 +10,6 @@ public class OriginalSlot : MonoBehaviour
     [SerializeField]
     private Transform originalParent;
 
-    public Transform getOriginalParent() {  return originalParent; }
+    public Transform getOriginalParent() { return originalParent; }
 
-    Vector3 originalPosition;
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        originalPosition = transform.localPosition;
-    }
-
-    public void resetPosition()
-    {
-        transform.localPosition = originalPosition;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
