7855334bf74a154c3184baaff16b584256dfb050
JustARandomGuyNumber13 <trivu2021@gmail.com>
2025-02-26 15:17:13 -0600

    Set up test scenes in Main Asset folder

--- Assets/Scripts/Background Effects/Background_Parallax_Effect.cs
+++ Assets/Scripts/Background Effects/Background_Parallax_Effect.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class Background_Parallax_Effect : MonoBehaviour
 {
-    [SerializeField] private Transform _cameraTransform;
     [SerializeField] private float _parallaxEffectMultiplierX = 0.5f; // Only X-axis multiplier
 
+    private Transform _cameraTransform;
     private Vector3 _lastCameraPosition;
     private float _initialYPosition; // Store the initial Y position
 
     void Start()
     {
+        _cameraTransform = Camera.main.transform;
         _lastCameraPosition = _cameraTransform.position;
         _initialYPosition = transform.position.y; // Store the initial Y
     }
