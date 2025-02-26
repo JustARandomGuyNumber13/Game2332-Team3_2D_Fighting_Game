dedc780fd95131206f0a42610d5357807706a993
A-Aron226 <aroeder345@gmail.com>
2025-02-26 11:57:10 -0600

    Updated players' health bars and will do more testing

--- Assets/Aaron/Assets and Scripts for Tri-Folder game scene/Scripts/HealthBar.cs
+++ Assets/Aaron/Assets and Scripts for Tri-Folder game scene/Scripts/HealthBar.cs
@@ -4,8 +4,18 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image fillImage;
+    [SerializeField] private PlayerHealthHandler handler;
 
-    public void SetHealth(float health, float maxHealth)
+    private float maxHealth;
+
+    public void Start()
+    {
+        maxHealth = handler._characterStat.maxHealth;
+        handler.OnHealthIncreaseEvent.AddListener(SetHealth);
+        handler.OnHealthDecreaseEvent.AddListener(SetHealth);
+        SetHealth(maxHealth);
+    }
+    public void SetHealth(float health)
     {
         fillImage.fillAmount = health / maxHealth;
     }
