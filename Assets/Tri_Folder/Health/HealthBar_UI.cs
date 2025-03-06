8d967d6c40dbbcc7067d94e316d92e88f7aba8dc
JustARandomGuyNumber13 <trivu2021@gmail.com>
2025-03-06 07:39:53 -0600

    Finish Game Manager basic

--- Assets/Tri_Folder/Health/HealthBar_UI.cs
+++ Assets/Tri_Folder/Health/HealthBar_UI.cs
@@ -6,11 +6,12 @@ public class HealthBar_UI : MonoBehaviour
     [SerializeField] private Image fillImage;
     private float maxHealth;
 
-    public void Public_SetUp(PlayerHealthHandler handler)
-    { 
-        maxHealth = handler.health;
-        handler.OnHealthIncreaseEvent.AddListener(SetHealth);
-        handler.OnHealthDecreaseEvent.AddListener(SetHealth);
+    public void Public_SetUp(GameObject healthHandler)
+    {
+        PlayerHealthHandler health = healthHandler.GetComponent<PlayerHealthHandler>();
+        maxHealth = health.health;
+        health.OnHealthIncreaseEvent.AddListener(SetHealth);
+        health.OnHealthDecreaseEvent.AddListener(SetHealth);
         SetHealth(maxHealth);
     }
     public void SetHealth(float health) // Call by Unity Event
