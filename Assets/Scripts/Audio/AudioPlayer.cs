a6104042650de8d87ea13db6d9123939e4676a84
A-Aron226 <aroeder345@gmail.com>
2025-03-28 19:56:49 -0500

    Testing audio for player selection scene

--- Assets/Scripts/Audio/AudioPlayer.cs
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
 
 [System.Serializable]
 public class SkillAudioMapping
@@ -11,15 +13,27 @@ public class SkillAudioMapping
     public AudioClip sfxClip;
 }
 
+[System.Serializable]
+public class ButtonAudioMapping
+{
+    public string actionMapName;
+    public AudioClip buttonClip;
+}
+
 public class AudioPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource _bgm, _sfx;
     [SerializeField] private AudioClip _menuBGM, _gameBGM, _selectionBGM;
     [SerializeField] private SkillAudioMapping[] skillAudioMappings;
+    [SerializeField] private InputActionReference p1MoveRight, p1MoveLeft, p1Confirm, p1GoBack, p1Ready;
+    [SerializeField] private InputActionReference p2MoveRight, p2MoveLeft, p2Confirm, p2GoBack, p2Ready;
+    [SerializeField] private AudioClip _p1MoveRClip, _p1MoveLClip, _p1ConfirmClip, _p1GoBackClip, _p1ReadyClip;
+    [SerializeField] private AudioClip _p2MoveRClip, _p2MoveLClip, _p2ConfirmClip, _p2GoBackClip, _p2ReadyClip;
 
     public static AudioPlayer _instance;
 
     private Dictionary<SO_SkillStat, AudioClip> _sfxMapping = new Dictionary<SO_SkillStat, AudioClip>();
+    private Dictionary<string, AudioClip> _buttonMapping = new Dictionary<string, AudioClip>();
 
     private void Awake()
     {
@@ -34,6 +48,7 @@ public class AudioPlayer : MonoBehaviour
         InitializeSFXMapping();
     }
 
+    //Audio Mapping
     private void InitializeSFXMapping()
     {
         foreach (var mapping in skillAudioMappings)
@@ -44,7 +59,7 @@ public class AudioPlayer : MonoBehaviour
             }
             else
             {
-                Debug.LogWarning("Skill or SFX is missing in mapping!");
+                Debug.LogWarning("Skill or SFX are not assigned!");
             }
         }
     }
@@ -52,11 +67,51 @@ public class AudioPlayer : MonoBehaviour
     private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
+
+        //Player 1
+        p1MoveRight.action.Enable();
+        p1MoveLeft.action.Enable();
+        p1Confirm.action.Enable();
+        p1GoBack.action.Enable();
+        p1Ready.action.Enable();
+
+        p1MoveRight.action.started += _ => Public_PlaySoundEffect(_p1MoveRClip);
+        p1MoveLeft.action.started += _ => Public_PlaySoundEffect(_p1MoveLClip);
+        p1Confirm.action.started += _ => Public_PlaySoundEffect(_p1ConfirmClip);
+        p1GoBack.action.started += _ => Public_PlaySoundEffect(_p1GoBackClip);
+        p1Ready.action.started += _ => Public_PlaySoundEffect(_p1ReadyClip);
+
+        //Player 2
+        p2MoveRight.action.Enable();
+        p2MoveLeft.action.Enable();
+        p2Confirm.action.Enable();
+        p2GoBack.action.Enable();
+        p2Ready.action.Enable();
+
+        p2MoveRight.action.started += _ => Public_PlaySoundEffect(_p2MoveRClip);
+        p2MoveLeft.action.started += _ => Public_PlaySoundEffect(_p2MoveLClip);
+        p2Confirm.action.started += _ => Public_PlaySoundEffect(_p2ConfirmClip);
+        p2GoBack.action.started += _ => Public_PlaySoundEffect(_p2GoBackClip);
+        p2Ready.action.started += _ => Public_PlaySoundEffect(_p2ReadyClip);
     }
 
     private void OnDisable()
     {
         SceneManager.sceneLoaded -= OnSceneLoaded;
+
+        //Player 1
+        p1MoveRight.action.Disable();
+        p1MoveLeft.action.Disable();
+        p1Confirm.action.Disable();
+        p1GoBack.action.Disable();
+        p1Ready.action.Disable();
+
+        //Player 2
+        p2MoveRight.action.Disable();
+        p2MoveLeft.action.Disable();
+        p2Confirm.action.Disable();
+        p2GoBack.action.Disable();
+        p2Ready.action.Disable();
     }
 
     private void OnSceneLoaded(Scene _scene, LoadSceneMode _mode)
@@ -69,8 +124,9 @@ public class AudioPlayer : MonoBehaviour
             case "Main-GamePlayer_Scene"://"Test 2-GamePlay":
                 Public_PlayBackgroundMusic(_gameBGM);
                 break;
-            /*case "";
-                Public_PlayBackgroundMusic(_selectionBGM);*/
+            case "Character-Skill Selection Scene":
+                Public_PlayBackgroundMusic(_selectionBGM);
+                break;
             default:
                 _bgm.Stop();
                 break;
@@ -98,4 +154,16 @@ public class AudioPlayer : MonoBehaviour
         _bgm.clip = audioClip;
         _bgm.Play();
     }
+
+    /*public void OnButtonPress(string inputActionName)
+    {
+        if (_buttonMapping.TryGetValue(inputActionName, out AudioClip iaClip))
+        {
+            Public_PlaySoundEffect(iaClip);
+        }
+        else
+        {
+            Debug.LogWarning($"No audio clip mapped for Input action: {inputActionName}");
+        }
+    }*/
 }
