05d10cfd3f4e33401b75ab21e8f6bd4de38f4b4b
A-Aron226 <aroeder345@gmail.com>
2025-04-08 16:11:07 -0500

    Audio fixing

--- Assets/Aaron/AudioMixer/AudioPlayer.cs
+++ Assets/Aaron/AudioMixer/AudioPlayer.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Windows;
 
 //Note: I am aware of the mess of this code and will try to clean it up when everything is completed
-//Note: Rework or Delete Defense and Jump Audio code. Current method is not working at the moment as player 2 actions somehow influence player 1 actions
 //Note: Move skills and selection IA audio to their respective player SFX method if possible
 
 
@@ -23,6 +22,19 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioSource _bgm, _sfx, _p1SFX, _p2SFX;
     [SerializeField] private AudioClip _menuBGM, _gameBGM, _selectionBGM;
     [SerializeField] private SkillAudioMapping[] skillAudioMappings;
+    private SO_SkillStat[] p1EquippedSkills = new SO_SkillStat[3];
+    private SO_SkillStat[] p2EquippedSkills = new SO_SkillStat[3];
+
+    //Player Skill Input Fields
+    [Header("Player 1 Skill Inputs")]
+    [SerializeField] private InputActionReference p1Skill1;
+    [SerializeField] private InputActionReference p1Skill2;
+    [SerializeField] private InputActionReference p1Skill3;
+
+    [Header("Player 2 Skill Inputs")]
+    [SerializeField] private InputActionReference p2Skill1;
+    [SerializeField] private InputActionReference p2Skill2;
+    [SerializeField] private InputActionReference p2Skill3;
 
     //Selection Scene input fields
     [Header("Player 1 Input Actions")]
@@ -31,10 +43,6 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private InputActionReference p1Confirm;
     [SerializeField] private InputActionReference p1GoBack;
     [SerializeField] private InputActionReference p1Ready;
-    
-    [SerializeField] private InputActionReference p1Jump;
-    [SerializeField] private InputActionReference p1Defend;
-    
 
     [Header("Player 2 Input Actions")]
     [SerializeField] private InputActionReference p2MoveRight;
@@ -43,9 +51,6 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private InputActionReference p2GoBack;
     [SerializeField] private InputActionReference p2Ready;
     
-    [SerializeField] private InputActionReference p2Jump;
-    [SerializeField] private InputActionReference p2Defend;
-    
 
     [Header("Player 1 IA Clips")]
     [SerializeField] private AudioClip _p1MoveRClip;
@@ -53,8 +58,6 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioClip _p1ConfirmClip;
     [SerializeField] private AudioClip _p1GoBackClip;
     [SerializeField] private AudioClip _p1ReadyClip;
-    //[SerializeField] private AudioClip _p1JumpClip;
-    //[SerializeField] private AudioClip _p1DefendClip;
     
 
     [Header("Player 2 IA Clips")]
@@ -63,22 +66,11 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] private AudioClip _p2ConfirmClip;
     [SerializeField] private AudioClip _p2GoBackClip;
     [SerializeField] private AudioClip _p2ReadyClip;
-    //[SerializeField] private AudioClip _p2JumpClip;
-    //[SerializeField] private AudioClip _p2DefendClip;
-
-    //[SerializeField] private float jumpClipCooldown = 0.85f;
     
 
     public static AudioPlayer _instance;
 
     private Dictionary<SO_SkillStat, AudioClip> _sfxMapping = new Dictionary<SO_SkillStat, AudioClip>();
-    /*
-    private Dictionary<InputActionReference, bool> IACooldown = new Dictionary<InputActionReference, bool>();
-    private List<PlayerInputHandler> playerInputHandlers = new List<PlayerInputHandler>();
-
-    private bool isP1DefendClipPlaying = false;
-    private bool isP2DefendClipPlaying = false;
-    */
 
     private void Awake()
     {
@@ -91,13 +83,6 @@ public class AudioPlayer : MonoBehaviour
         _instance = this;
         DontDestroyOnLoad(gameObject);
         InitializeSFXMapping();
-    }
-
-    private void Start()
-    {
-        //Cooldown states for basic actions
-        //IACooldown[p1Jump] = true;
-        //IACooldown[p2Jump] = true;
     }
 
     //Audio Mapping
@@ -116,6 +101,93 @@ public class AudioPlayer : MonoBehaviour
         }
     }
 
+    private void BindSkillInput()
+    {
+        EnableIA();
+
+        //Player 1
+        p1Skill1.action.performed += _ => PlaySkillAudio("Player 1", 0);
+        p1Skill2.action.performed += _ => PlaySkillAudio("Player 1", 1);
+        p1Skill3.action.performed += _ => PlaySkillAudio("Player 1", 2);
+
+        //Player 2
+        p2Skill1.action.performed += _ => PlaySkillAudio("Player 2", 0);
+        p2Skill2.action.performed += _ => PlaySkillAudio("Player 2", 1);
+        p2Skill3.action.performed += _ => PlaySkillAudio("Player 2", 2);
+    }
+
+    private void UnbindSkillInput()
+    {
+        DisableIA();
+
+        //Player 1
+        p1Skill1.action.performed -= _ => PlaySkillAudio("Player 1", 0);
+        p1Skill2.action.performed -= _ => PlaySkillAudio("Player 1", 1);
+        p1Skill3.action.performed -= _ => PlaySkillAudio("Player 1", 2);
+
+        //Player 2
+        p2Skill1.action.performed -= _ => PlaySkillAudio("Player 2", 0);
+        p2Skill2.action.performed -= _ => PlaySkillAudio("Player 2", 1);
+        p2Skill3.action.performed -= _ => PlaySkillAudio("Player 2", 2);
+    }
+
+    private void PlaySkillAudio(string playerName, int skillIndex)
+    {
+        SO_SkillStat skill = null;
+
+        if (playerName == "Player 1")
+        {
+            skill = p1EquippedSkills[skillIndex];
+        }
+        else if (playerName == "Player 2")
+        {
+            skill = p2EquippedSkills[skillIndex];
+        }
+
+        if (skill != null && _sfxMapping.TryGetValue(skill, out AudioClip skillClip))
+        {
+            Debug.Log($"{playerName} triggered skill {skill.name} with audio {skillClip.name}");
+
+            if (playerName == "Player 1")
+            {
+                Public_PlayP1SoundEffect(skillClip);
+            }
+            else if (playerName == "Player 2")
+            {
+                Public_PlayP2SoundEffect(skillClip);
+            }
+            else
+            {
+                Debug.LogWarning($"No SFX found for {playerName} skill at index {skillIndex}");
+            }
+        }
+    }
+
+    public void AssignSkillIndex(string playername, int skillIndex, SO_SkillStat skill)
+    {
+        if (playername == "Player 1")
+        {
+            p1EquippedSkills[skillIndex] = skill;
+        }
+        else if (playername == "Player 2")
+        {
+            p2EquippedSkills[skillIndex] = skill;
+        }
+        Debug.Log($"{playername} equipped {skill.name} in slot {skillIndex}");
+    }
+    /*private SO_SkillStat GetSkillStat(string playerName, int skillIndex)
+    {
+        if (playerName == "Player 1")
+        {
+            return skillAudioMappings[skillIndex].skillStat;
+        }
+
+        if (playerName == "Player 2")
+        {
+            return skillAudioMappings[skillIndex].skillStat;
+        }
+        return null;
+    }*/
     private void OnEnable()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -176,6 +248,9 @@ public class AudioPlayer : MonoBehaviour
         p1GoBack.action.Enable();
         p1Ready.action.Enable();
 
+        p1Skill1.action.Enable();
+        p1Skill2.action.Enable();
+        p1Skill3.action.Enable();
 
         //Player 2
         p2MoveRight.action.Enable();
@@ -183,6 +258,11 @@ public class AudioPlayer : MonoBehaviour
         p2Confirm.action.Enable();
         p2GoBack.action.Enable();
         p2Ready.action.Enable();
+
+        p2Skill1.action.Enable();
+        p2Skill2.action.Enable();
+        p2Skill3.action.Enable();
+
     }
 
     private void DisableIA()
@@ -194,172 +274,22 @@ public class AudioPlayer : MonoBehaviour
         p1GoBack.action.Disable();
         p1Ready.action.Disable();
 
+        p1Skill1.action.Disable();
+        p1Skill2.action.Disable();
+        p1Skill3.action.Disable();
+
         //Player 2
         p2MoveRight.action.Disable();
         p2MoveLeft.action.Disable();
         p2Confirm.action.Disable();
         p2GoBack.action.Disable();
         p2Ready.action.Disable();
-    }
+
+        p2Skill1.action.Disable();
+        p2Skill2.action.Disable();
+        p2Skill3.action.Disable();
+    }    
     
-    //Audio IA for basic actions
-    /*
-    private IEnumerator IAGameCooldown(InputActionReference action)
-    {
-        IACooldown[action] = false; //disables specific audio
-        yield return new WaitForSeconds(jumpClipCooldown); //waits for a certain amount of time
-        IACooldown[action] = true; //reenables specific audio
-    }
-    private void EnableGameMapping()
-    {
-        EnableGameIA();
-
-        //Player 1 Jump
-        p1Jump.action.performed += _ =>
-        {
-            Debug.Log("Player 1 jump triggered");
-            if (IACooldown[p1Jump])
-            {
-                Public_PlayP1SoundEffect(_p1JumpClip);
-                StartCoroutine(IAGameCooldown(p1Jump));
-            }
-        };
-
-        //Player 2 Jump
-        p2Jump.action.performed += _ =>
-        {
-            Debug.Log("Player 2 jump triggered");
-            if (IACooldown[p2Jump])
-            {
-                Public_PlayP2SoundEffect(_p2JumpClip);
-                StartCoroutine(IAGameCooldown(p2Jump));
-            }
-        };
-
-        //Defend
-        foreach (var playerHandler in playerInputHandlers)
-        {
-            if (playerHandler == null) continue;
-            //Player 1 Defend
-            p1Defend.action.performed += _ =>
-            {
-                Debug.Log($"Player 1 defend performed: isDefending = {playerHandler.isDefending}");
-                if (playerHandler.isDefending && !isP1DefendClipPlaying)
-                {
-                    Public_PlayP1SoundEffect(_p1DefendClip);
-                    isP1DefendClipPlaying = true;
-                }
-            };
-
-            p1Defend.action.canceled += _ =>
-            {
-                isP1DefendClipPlaying = false;
-            };
-
-            //Player 2 Defend
-            p2Defend.action.performed += _ =>
-            {
-                Debug.Log($"Player 2 defend performed: isDefending = {playerHandler.isDefending}");
-                if (playerHandler.isDefending && !isP2DefendClipPlaying)
-                {
-                    Public_PlayP2SoundEffect(_p2DefendClip);
-                    isP2DefendClipPlaying = true;
-                }
-            };
-
-            p2Defend.action.canceled += _ =>
-            {
-                isP2DefendClipPlaying = false;
-            };
-        }
-    }
-
-    private void DisableGameMapping()
-    {
-        DisableGameIA();
-        isP1DefendClipPlaying = false;
-        isP2DefendClipPlaying = false;
-
-        //Player 1 Jump
-        p1Jump.action.performed -= _ => 
-        {
-            Debug.Log("Player 1 jump unbounded");
-            if (IACooldown[p1Jump]) 
-            { 
-                Public_PlayP1SoundEffect(_p1JumpClip);
-                StartCoroutine(IAGameCooldown(p1Jump));
-            } 
-        };
-
-        //Player 2 Jump
-        p2Jump.action.performed -= _ =>
-        {
-            Debug.Log("Player 2 jump unbounded");
-            if (IACooldown[p2Jump])
-            {
-                Public_PlayP2SoundEffect(_p2JumpClip);
-                StartCoroutine(IAGameCooldown(p2Jump));
-            }
-        };
-
-        //Defend
-        foreach (var playerHandler in playerInputHandlers)
-        {
-            if (playerHandler == null) continue;
-            //Player 1 Defend
-            p1Defend.action.performed -= _ =>
-            {
-                if (playerHandler.isDefending && !isP1DefendClipPlaying)
-                {
-                    Public_PlayP1SoundEffect(_p1DefendClip);
-                    isP1DefendClipPlaying = true;
-                }
-            };
-
-            p1Defend.action.canceled -= _ =>
-            {
-                isP1DefendClipPlaying = false;
-            };
-
-            //Player 2 Defend
-            p2Defend.action.performed -= _ =>
-            {
-                if (playerHandler.isDefending && !isP2DefendClipPlaying)
-                {
-                    Public_PlayP2SoundEffect(_p2DefendClip);
-                    isP2DefendClipPlaying = true;
-                }
-            };
-
-            p2Defend.action.canceled -= _ =>
-            {
-                isP2DefendClipPlaying = false;
-            };
-        }  
-    }
-
-    private void EnableGameIA()
-    {
-        //Player 1
-        p1Jump.action.Enable();
-        p1Defend.action.Enable();
-
-        //Player 2
-        p2Jump.action.Enable();
-        p2Defend.action.Enable();
-    }
-
-    private void DisableGameIA()
-    {
-        //Player 1
-        p1Jump.action.Disable();
-        p1Defend.action.Disable();
-
-        //Player 2
-        p2Jump.action.Disable();
-        p2Defend.action.Disable();
-    }
-    */
     private void OnSceneLoaded(Scene _scene, LoadSceneMode _mode)
     {
         switch (_scene.name)
@@ -388,17 +318,16 @@ public class AudioPlayer : MonoBehaviour
         {
             DisableAudioMapping();
         }
-        
-        //Ensuring that audio clips assigned only play if the scene is the game scene
-        /*if (_scene.name == "Main-GamePlayer_Scene")
+
+        if ( _scene.name == "Main-GamePlayer_Scene")
         {
-            EnableGameMapping();
+            BindSkillInput();
         }
 
         else
         {
-            DisableGameMapping();
-        }*/
+            UnbindSkillInput();
+        }
     }
 
     //Play sounds
@@ -408,14 +337,14 @@ public class AudioPlayer : MonoBehaviour
         _sfx.Play();
     }
 
-    //Player 1 basic action play sound
+    //Player 1
     public void Public_PlayP1SoundEffect(AudioClip audioClip)
     {
         _p1SFX.clip = audioClip;
         _p1SFX.Play();
     }
 
-    //Player 2 basic action play sound
+    //Player 2
     public void Public_PlayP2SoundEffect(AudioClip audioClip)
     {
         _p2SFX.clip = audioClip;
