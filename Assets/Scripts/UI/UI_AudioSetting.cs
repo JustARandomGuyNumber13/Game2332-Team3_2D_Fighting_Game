fc460e21a8951ffdc146c739be749b220c1ee4dc
JustARandomGuyNumber13 <trivu2021@gmail.com>
2025-04-10 11:08:59 -0500

    Aaron merge

--- Assets/Scripts/UI/UI_AudioSetting.cs
+++ Assets/Scripts/UI/UI_AudioSetting.cs
@@ -13,11 +13,6 @@ public class UI_AudioSetting : MonoBehaviour
     [SerializeField] private Slider _bgmSlider;
     [SerializeField] private Slider _sfxSlider;
 
-    [SerializeField] private Toggle _masterToggle;
-    [SerializeField] private Toggle _bgmToggle;
-    [SerializeField] private Toggle _sfxToggle;
-
-
     private void Start()
     {
         SetUp();
@@ -26,37 +21,55 @@ public class UI_AudioSetting : MonoBehaviour
     {
         float masterVolume = 0;
         _mixer.GetFloat("Master", out masterVolume);
-        _masterSlider.value = masterVolume;
+        _masterSlider.value = DecibelToLinear(masterVolume);
         _masterSlider.onValueChanged.AddListener(SetMasterVolume);
 
         float bgmVolume = 0;
         _mixer.GetFloat("Bgm", out bgmVolume);
-        _bgmSlider.value = bgmVolume;
+        _bgmSlider.value = DecibelToLinear(bgmVolume);
         _bgmSlider.onValueChanged.AddListener(SetBgmVolume);
 
         float sfxVolume = 0;
         _mixer.GetFloat("Sfx", out sfxVolume);
-        _sfxSlider.value = sfxVolume;
+        _sfxSlider.value = DecibelToLinear(sfxVolume);
         _sfxSlider.onValueChanged.AddListener(SetSfxVolume);
     }
     public void UI_OnReturnButtonPress()
     {
 
         if (SceneManager.GetActiveScene().name == "Test 1-MainMenu")
-            SceneManager.LoadScene("Test 2-GamePlay");
+            //SceneManager.LoadScene("Test 2-GamePlay");
+            //SceneManager.LoadScene("Character-Skill Selection Scene");
+            SceneManager.LoadScene("Main-GamePlayer_Scene");
         else if (SceneManager.GetActiveScene().name == "Test 2-GamePlay")
             SceneManager.LoadScene("Test 1-MainMenu");
     }
     private void SetMasterVolume(float value)
     {
-        _mixer.SetFloat("Master", value * Mathf.Log10(value) * 20);
+        //_mixer.SetFloat("Master", value * Mathf.Log10(value) * 20);
+        _mixer.SetFloat("Master", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20);
     }
     private void SetBgmVolume(float value)
     {
-        _mixer.SetFloat("Bgm", value * Mathf.Log10(value) * 20);
+        //_mixer.SetFloat("Bgm", value * Mathf.Log10(value) * 20);
+        _mixer.SetFloat("Bgm", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20);
     }
     private void SetSfxVolume(float value)
     {
-        _mixer.SetFloat("Sfx", value * Mathf.Log10(value) * 20);
+        //_mixer.SetFloat("Sfx", value * Mathf.Log10(value) * 20);
+        _mixer.SetFloat("Sfx", Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20);
+    }
+
+    private void OnDestroy()
+    {
+        _masterSlider.onValueChanged.RemoveListener(SetMasterVolume);
+        _bgmSlider.onValueChanged.RemoveListener(SetBgmVolume);
+        _sfxSlider.onValueChanged.RemoveListener(SetSfxVolume);
+
+    }
+
+    private float DecibelToLinear(float decibel)
+    {
+        return Mathf.Pow(10, decibel / 20);
     }
 }
