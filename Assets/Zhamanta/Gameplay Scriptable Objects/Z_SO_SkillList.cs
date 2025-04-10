e345f92ce129dfe730af1e03285dc4d2f910c9ba
Zhamanta Chavez <zhamantach04@gmail.com>
2025-04-10 10:55:13 -0500

    Cleaned Up Zhamanta Folder

--- Assets/Zhamanta/Gameplay Scriptable Objects/Z_SO_SkillList.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Z_SO_Skill_List", menuName = "Scriptable Objects/Z_SO_Skill_List")]
-public class Z_SO_SkillList : ScriptableObject
-{
-    public Z_SO_SkillStat[] skillList;
-}
