073d3e5b4349fc949a60f39e1aee37c22bf8c5cf
Zhamanta Chavez <zhamantach04@gmail.com>
2025-04-15 10:59:56 -0500

    Key-Instructions Connected

--- Assets/Zhamanta/Scripts/Selection UI/KeyInstructions.cs
+++ Assets/Zhamanta/Scripts/Selection UI/KeyInstructions.cs
@@ -9,10 +9,12 @@ public class KeyInstructions : MonoBehaviour
     [Header("Player 1")]
     [SerializeField] private TMP_Text _p1ChooseText;
     [SerializeField] private TMP_Text _p1ReadyText;
+    [SerializeField] private TMP_Text _p1BackText;
 
     [Header("Player 2")]
     [SerializeField] private TMP_Text _p2ChooseText;
     [SerializeField] private TMP_Text _p2ReadyText;
+    [SerializeField] private TMP_Text _p2BackText;
 
     private void Start()
     {
@@ -25,13 +27,14 @@ public class KeyInstructions : MonoBehaviour
         InputActionMap _p1InputMap = _actionAsset.FindActionMap("Player1");
         InputActionMap _p2InputMap = _actionAsset.FindActionMap("Player2");
 
-        SetUpInstruction(_p1ChooseText, _p1ReadyText, _p1InputMap);
-        SetUpInstruction(_p2ChooseText, _p2ReadyText, _p2InputMap);
+        SetUpInstruction(_p1ChooseText, _p1ReadyText, _p1BackText, _p1InputMap);
+        SetUpInstruction(_p2ChooseText, _p2ReadyText, _p2BackText, _p2InputMap);
     }
 
-    private void SetUpInstruction(TMP_Text chooseText, TMP_Text readyText, InputActionMap map)
+    private void SetUpInstruction(TMP_Text chooseText, TMP_Text readyText, TMP_Text backText, InputActionMap map)
     {
         chooseText.text += map.FindAction("Attack").GetBindingDisplayString();
         readyText.text += map.FindAction("SkillOne").GetBindingDisplayString();
+        backText.text += map.FindAction("SkillOne").GetBindingDisplayString();
     }
 }
